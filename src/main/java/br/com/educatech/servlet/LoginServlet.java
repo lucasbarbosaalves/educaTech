be82a764b105fc60c9fef78b6b542cc2e9b94116
lukas <lkab05@hotmail.com>
2023-10-14 12:57:14 -0300

    add login/logout logic and authentication filter

---
+++ src/main/java/br/com/educatech/servlet/LoginServlet.java
@@ -0,0 +1,49 @@
+package br.com.educatech.servlet;
+
+import br.com.educatech.dao.StudentDao;
+import br.com.educatech.models.Student;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String username = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        Student user = new Student(username, password);
+
+        boolean isValidUser = new StudentDao().verifyCredentials(user);
+
+        if (isValidUser) {
+
+            req.getSession().setAttribute("loggedUser", username);
+
+            resp.sendRedirect("find-all-students");
+
+        } else {
+
+            req.setAttribute("message", "Invalid credentials!");
+
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+
+        }
+
+    }
+
+}
